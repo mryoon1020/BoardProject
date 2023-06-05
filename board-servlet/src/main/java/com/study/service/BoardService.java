08e585f34e27298e720955493b4731e608066334
Deuk_gu_Yoon <deukgu1020@naver.com>
2023-06-05 22:45:51 +0900

    update

--- board-servlet/src/main/java/com/study/service/BoardService.java
+++ board-servlet/src/main/java/com/study/service/BoardService.java
@@ -4,5 +4,5 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface BoardService {
-    String runService(HttpServletRequest request, HttpServletResponse response);
+    void runService(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
